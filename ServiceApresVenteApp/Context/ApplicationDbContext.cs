bb7d016c25af840fdc2c81095a6a6d77d986994e
TheJoke <cherif.youssef@enis.tn>
2025-01-04 23:22:13 +0100

    first commit

---
+++ ServiceApresVenteApp/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ServiceApresVente.Models;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Article> Articles { get; set; }
+    public DbSet<Reclamation> Reclamations { get; set; }
+    public DbSet<Intervention> Interventions { get; set; }
+    public DbSet<PieceDeRechange> Pieces { get; set; }
+    public DbSet<ResponsableSAV> Responsables { get; set; }
+}
