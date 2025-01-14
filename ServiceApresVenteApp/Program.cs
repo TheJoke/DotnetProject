2407bf457639a3c73b6ec61d6b59c5350cfd6fcc
MAmir20 <mezghani.amir@gmail.com>
2025-01-14 13:27:25 +0100

    updated default route

--- ServiceApresVenteApp/Program.cs
+++ ServiceApresVenteApp/Program.cs
@@ -39,6 +39,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
